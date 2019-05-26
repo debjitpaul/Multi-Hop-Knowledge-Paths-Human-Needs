23930180cfddd21589e94af9231b58820bcb690f
Debjit Paul <deb_4u22@yahoo.com>
2019-05-26 14:37:17 +0200

    Update run_extraction.sh

--- src/graph_model/run_extraction.sh
+++ src/graph_model/run_extraction.sh
@@ -4,5 +4,5 @@ for file in /subgraph/train/*
 do
   BASENAME=$(basename $file)
   echo $BASENAME
-  python extract_path.py --graph_path /Users/debjit/Downloads/data/storycommonsense_data/subgraph/train/$BASENAME* -input_path input.txt -output_path output.txt --purpose train
+  python extract_path.py --graph_path /Users/debjit/Downloads/data/storycommonsense_data/subgraph/train/$BASENAME* --input_path input.txt --output_path output.txt --purpose train
 done
